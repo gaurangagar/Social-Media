be936fd55d9a1e9b2cb9e870a6c465ca18eabac1
Gaurang Agarwal <gaurangagarwal26@gmail.com>
2025-02-01 17:43:11 +0530

    context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useState,useContext,useEffect } from 'react'
 import './App.css'
+import Login from './pages/Login.jsx'
+import Home from './pages/Home.jsx'
+import { setLocalStorage  } from './utils/LocalStorage.jsx'
+import { AuthContext } from './Context/AuthProvider.jsx'
 
 function App() {
+  useEffect(() => {
+    setLocalStorage()
+  }, [])
+
+  const [user,setUser]=useState('')
+
+  const {userData,setuserData}=useContext(AuthContext)
+  console.log(userData)
+
+  const handleLogin=(email,password)=>{
+    setUser('user')
+  }
 
   return (
-    <>
-      <div>
-        <h1>Social media app</h1>
-      </div>
-    </>
+    <div>
+      {user=='' ? <Login handleLogin={handleLogin}/> : null}
+      {user=='user' ? <Home/> : null}
+    </div>
   )
 }
 

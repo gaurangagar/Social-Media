54c1c388ef0160d5a07bea86f95b6bf69c44a64a
Gaurang Agarwal <gaurangagarwal26@gmail.com>
2025-02-01 15:21:35 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Social media app</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
